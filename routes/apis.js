0bd022fb10860bd79deed71b73aac937d448d0e8
raj vaibhav singh <rajvaibhavsingh2310@gmail.com>
2017-03-17 22:51:59 +0530

    Added mock data

--- routes/apis.js
+++ routes/apis.js
@@ -22,6 +22,33 @@ function readJSONFile(filename, callback) {
 }
 
 
+router.get('/impl',(req,res)=>{
+	getUserLists(function(users){
+		let obj={};
+		obj.available_users=users.filter((user)=>user.team_name===null);
+		let map=new Map();
+		for(let user of users){
+			if (user.team_name!=null) {
+			//	console.log(user);
+			
+				if(!(map).has(user.team_name)){
+						map.set(user.team_name,[]);
+				}
+					map.set(user.team_name,(map.get(user.team_name)).push(user));
+				
+				/*if ((obj.teams).get(user.team_name)) {
+					(obj.teams).set(user.team_name,((obj.teams).get(user.team_name)).push(obj));
+				} else {
+					(obj.teams).set(user.team_name,[user]);
+				}*/
+				
+			}
+		}
+		console.log(map);
+		res.send(obj);
+	});
+});
+
 router.get('/userRatings', (req, res) => {
 	readJSONFile('commit_data.json', function (err, json) {
 	  if(err) { throw err; }
@@ -44,10 +71,15 @@ function getUserLists(callback){
    MongoClient.connect(database_url,function(err, db) {
       db.collection('users', function(err, collection) {
         if(err){
+        	console.log("err"+err);
             return;
         }
         else {
-          collection.find({}).toArray(function(err, items) {
+          collection.find({}).limit(90).toArray(function(err1, items) {
+          	if(err1){
+          		console.log(err1);
+          	}
+              //console.log(items);
               callback(items);
           });
         }
