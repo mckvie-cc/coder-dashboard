d96c0d2f506430df49155ee37f3c0bec44ca5971
Ankit Gaurav <ankitgaurav@outlook.com>
2017-03-17 23:18:42 +0530

    Remove unnecessory code

--- routes/apis.js
+++ routes/apis.js
@@ -2,38 +2,11 @@ var express = require('express')
 var router = express.Router()
 const json_data = require('../commit_data.json')
 
-router.get('/impl', (req, res) => {
-    getUserLists(function(users) {
-        let obj = {};
-        obj.available_users = users.filter((user) => user.team_name === null);
-        let map = new Map();
-        for (let user of users) {
-            if (user.team_name != null) {
-                //	console.log(user);
-
-                if (!(map).has(user.team_name)) {
-                    map.set(user.team_name, []);
-                }
-                map.set(user.team_name, (map.get(user.team_name)).push(user));
-
-                /*if ((obj.teams).get(user.team_name)) {
-                	(obj.teams).set(user.team_name,((obj.teams).get(user.team_name)).push(obj));
-                } else {
-                	(obj.teams).set(user.team_name,[user]);
-                }*/
-
-            }
-        }
-        console.log(map);
-        res.send(obj);
-    });
-});
-
 router.get('/userRatings', (req, res) => {
     let db = req.db
     db.collection('users', function(err, collection) {
         if (err) {
-            throw new Error(err1)
+            throw new Error(err)
             return;
         } else {
             collection.find({}).toArray(function(err, user_list) {
