92a57e9e36cd5bc35788810d7444249cab65cbfa
Ankit Gaurav <ankitgaurav@outlook.com>
2017-03-26 12:07:10 +0530

    Rename index.js to app.js, change procfile, package json

--- routes/web_pages.js
+++ routes/web_pages.js
@@ -2,15 +2,15 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {title: 'Coder-Dashboard | Home'})
 })
 router.get('/signup', (req, res)=>{
-	res.render('signup')
+	res.render('signup', {title: 'Coder-Dashboard | Register'})
 })
 router.get('/login', (req, res)=>{
-	res.render('login')
+	res.render('login', {title: 'Coder-Dashboard | Login'})
 })
 router.get('/profile', (req, res)=>{
-	res.render('profile')
+	res.render('profile', {title: 'Coder-Dashboard | Profile'})
 })
 module.exports = router
